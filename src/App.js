566231c2db1a7703fef763630b703ba6c6d340f8
Lance Hudson <lancexhudson@gmail.com>
2025-11-14 08:49:55 -0500

    update useState on App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import SavingsRateCalculator from './components/SavingsRateCalculator';
 import RetirementCalculator from './components/RetirementCalculator';
